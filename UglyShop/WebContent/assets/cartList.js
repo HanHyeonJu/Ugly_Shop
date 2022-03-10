1762d009dd7afd781662e170268480b3529018b3
HanHyeonJu <94498825+HanHyeonJu@users.noreply.github.com>
2022-03-10 12:52:02 +0900

    3월 10일 카트 수량,삭제 오류 수정

--- UglyShop/WebContent/assets/cartList.js
+++ UglyShop/WebContent/assets/cartList.js
@@ -3,10 +3,10 @@
 (function () {
   //수량 변경 (up버튼을 누름)
   $('.up').on('click', function (e) {
-    let param = $(this).attr('data-id');
+    let id = $(this).attr('data-id');
     $.ajax({
       type: 'GET',
-      url: 'http://localhost:8090/SHOP' + '/cart?id=' + param,
+      url: 'http://localhost:8090/UglyShop' + '/cart?id=' + id,
       data: $('.up').serialize(),
     }).done(function (data) {
       console.log(data);
@@ -16,10 +16,10 @@
 
   // 수량 변경 (down 버튼을 누름)
   $('.down').on('click', function (e) {
-    let param = $(this).attr('data-id');
+    let id = $(this).attr('data-id');
     $.ajax({
       type: 'POST',
-      url: 'http://localhost:8090/SHOP' + '/cart?id=' + param,
+      url: 'http://localhost:8090/UglyShop' + '/cart?id=' + id,
       data: $('.down').serialize(),
     }).done(function (data) {
       console.log(data);
@@ -29,10 +29,10 @@
 
   // 삭제
   $('#delete').on('click', function (e) {
-    let param = $(this).attr('data-id');
+    let id = $(this).attr('data-id');
     $.ajax({
       type: 'POST',
-      url: 'http://localhost:8090/SHOP' + '/cart?cmd=delete&id=' + param,
+      url: 'http://localhost:8090/UglyShop' + '/cart?cmd=delete&id=' + id,
       data: {},
     }).done(function (data) {
       console.log(data);
